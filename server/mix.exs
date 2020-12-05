cf6c5a190d61ac7c8ed9d1a0a5c7baaac477a19d
Jhefrey Sajot <heifurukawa1997@gmail.com>
2020-12-05 21:35:07 +0800

    add phoenix graphql library

--- server/mix.exs
+++ server/mix.exs
@@ -42,7 +42,12 @@ defmodule Prepply.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.5"},
+      {:absinthe_plug, "~> 1.5"},
+      {:absinthe_phoenix, "~> 2.0"},
+      {:dataloader, "~> 1.0.0"},
+      {:argon2_elixir, "~> 2.3"}
     ]
   end
 
