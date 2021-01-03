b9505b472d7fbbc04dd2090ade950218e8b72b0c
Jhefrey Sajot <heifurukawa1997@gmail.com>
2021-01-03 13:29:49 +0800

    seeds initial data

--- server/priv/repo/seeds.exs
+++ server/priv/repo/seeds.exs
@@ -9,3 +9,35 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Prepply.Onboarding
+alias Prepply.Employees
+
+checklist = %{
+  "name" => "Default",
+  "items" => [%{"name" => "NBI Clearance"}, %{"name" => "Postal ID"}]
+}
+
+Onboarding.create_checklist(checklist)
+
+acc1 = %{
+  "username" => "j.sajot",
+  "password" => "test1234",
+  "email" => "j.sajot@gmail.com",
+  "name" => "Jhefrey Sajot",
+  "mobile_number" => "09290393532",
+  "template_id" => "1"
+}
+
+Employees.create_employee(acc1)
+
+acc2 = %{
+  "username" => "j.sajot2",
+  "password" => "test1234",
+  "email" => "j.sajot@gmail.com",
+  "name" => "Sajot Jhefrey",
+  "mobile_number" => "09290393532",
+  "template_id" => "1"
+}
+
+Employees.create_employee(acc2)
