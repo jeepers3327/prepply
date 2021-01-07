d97e14d5361840b6117345a0ac9f04bfcf68c3a6
Jhefrey Sajot <heifurukawa1997@gmail.com>
2021-01-07 20:22:07 +0800

    update model file

--- server/lib/prepply/accounts/user.ex
+++ server/lib/prepply/accounts/user.ex
@@ -7,6 +7,8 @@ defmodule Prepply.Accounts.User do
     field :password, :string, virtual: true
     field :role, :string
     field :username, :string
+    field :email, :string
+    field :reset_password_token, :string
 
     has_one :employee_profile, Prepply.Employees.EmployeeProfile
 
@@ -16,10 +18,15 @@ defmodule Prepply.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:username, :password, :role])
-    |> validate_required([:username, :password])
-    |> unique_constraint([:username])
+    |> cast(attrs, [:username, :password, :role, :email])
+    |> validate_required([:username, :password, :email])
+    |> unique_constraint([:username, :email])
     |> put_password_hash()
+  end
+
+  def reset_password_changeset(user, attrs) do
+    user
+    |> cast(attrs, [:reset_password_token])
   end
 
   defp put_password_hash(
--- server/lib/prepply/employees/employee_profile.ex
+++ server/lib/prepply/employees/employee_profile.ex
@@ -3,7 +3,6 @@ defmodule Prepply.Employees.EmployeeProfile do
   import Ecto.Changeset
 
   schema "employee_profiles" do
-    field :email, :string
     field :mobile_number, :string
     field :name, :string
     belongs_to :user, Prepply.Accounts.User
@@ -15,7 +14,7 @@ defmodule Prepply.Employees.EmployeeProfile do
   @doc false
   def changeset(employee_profile, attrs) do
     employee_profile
-    |> cast(attrs, [:name, :mobile_number, :email])
-    |> validate_required([:name, :mobile_number, :email])
+    |> cast(attrs, [:name, :mobile_number])
+    |> validate_required([:name, :mobile_number])
   end
 end
